69469db4b75a40d2088084b0cb8b00f8a4b890e4
contactandy <contactandy@duck.com>
2024-08-01 06:11:25 -0400

    Add routines for working with nested JSON
    Wireshark packet details exported to JSON are heavily nested. These
    routines provide ways to more easily traverse deep JSON or to
    quickly find a specific type of object in a JSON

--- wireshark_digest_to_sqlite/digest.py
+++ wireshark_digest_to_sqlite/digest.py
@@ -1,5 +1,8 @@
 """Provide utility functions for working with deeply nested JSONs."""
 
+import itertools
+
+
 def nodes(json_data):
     """Return iterable of all leaf nodes (non-dict and non-array) in a JSON."""
     to_visit = None
@@ -28,27 +31,32 @@ def labels(json_data):
         pass
 
 
+def strip_matching_prefix(to_strip, to_match_prefix):
+    """Return to_strip with any shared prefix of to_match_prefix removed."""
+    letter_pairs = zip(to_strip, to_match_prefix)
+    iterate_to_no_match = itertools.takewhile(
+        lambda pair: pair[0] == pair[1], letter_pairs
+    )
+    matching_prefix = "".join(pair[0] for pair in iterate_to_no_match)
+    return to_strip.removeprefix(matching_prefix)
+
+
 class JsonObject:
     """Provide alternative to dict in reading JSON objects.
-    
+
     When reading or loading a JSON object, use this as an alternative to dicts
     to be able to access values through attributes instead of keys. Can be used
     with `object_hook` for json.load.
 
     Subclass this for "types" of objects (e.g. an array of JSON objects of
     similar structure) that may require additional processing, attributes, or
-    methods. 
+    methods.
     """
-    def __init__(self, /, **kwargs):
-        """Provide each attribute as a keyword argument."""
-        self.__dict__.update(kwargs)
-        self._json_raw = kwargs
 
-    
-    @classmethod
-    def from_dict(cls, dictionary):
-        """Construct from a dict instead of keywords."""
-        return cls(**dictionary)
+    def __init__(self, obj_dict):
+        """Initialize with a dict of attribute, value pairs."""
+        self.__dict__.update(obj_dict)
+        self._json_raw = obj_dict
 
     def __repr__(self):
         """Return a string with the defining key/value pairs."""
@@ -67,25 +75,63 @@ class JsonObject:
         """Return if self has the same defining key/value pairs as other."""
         return self._json_raw == other._json_raw
 
+    def promote(self, subclass, val_for_prefix_strip=None):
+        """Return a subclass instance formed from the original input to self.
 
-def promote_named_objects(obj, object_map):
+        Return a subclass instance created by calling the subclass.__init__ on
+        the argument this self was initialized with. In some use cases, the
+        keys of the original data need to be renamed such that a common prefix
+        of those keys is removed.
+        """
+        if val_for_prefix_strip:
+            promoted = subclass(
+                {
+                    strip_matching_prefix(key, val_for_prefix_strip): value
+                    for key, value in self._json_raw.items()
+                }
+            )
+        else:
+            promoted = subclass(self._json_raw)
+        return promoted
+
+
+def promote_named_objects(obj, object_map, keep_dots=False, strip_prefixes=False):
     """Convert JsonObjects to new classes by key names.
 
     For each label in obj that is in object_map, promote the value(s) at key
     from the generic JsonObject to the class identified by object_map. Then
     return JsonObject(obj). Can be used with `object_hook` when loading JSON.
+    See testing of this function for an example that avoids lambda functions by
+    using functools.partial to provide the object_map.
+
+    Keys with '.'s are inconvenient because they cannot be accessed directly as
+    attributes (see object.a.b vs getattr(object, "a.b")). When keep_dots is
+    False, '.'s in keys are replaced with '_'s.
+
+    A common use case with nested json objects carries the key name for an
+    object into the keys of the object itself. To automatically remove this
+    redundancy, use the strip_prefixes option.
     """
+    # can't rename keys mid for-loop so need a destination for the changes
+    dst_obj = dict()
+
     for key, preprocessed in obj.items():
-        promo_cls = object_map.get(key)
-        if not promo_cls:
-            pass
-        elif isinstance(preprocessed, JsonObject):
-            obj[key] = promo_cls(**preprocessed._json_raw)
+        promo_cls = object_map.get(key, JsonObject)
+        delimeter = "." if keep_dots else "_"
+        new_key = key if keep_dots else key.replace(".", "_")
+        strip_key = f"{new_key}{delimeter}" if strip_prefixes else None
+        if isinstance(preprocessed, JsonObject):
+            reprocessed = preprocessed.promote(promo_cls, strip_key)
         elif isinstance(preprocessed, list):
-            obj[key] = [
-                promo_cls(**entry._json_raw) if isinstance(entry, JsonObject) else entry
+            reprocessed = [
+                (
+                    entry.promote(promo_cls, strip_key)
+                    if isinstance(entry, JsonObject)
+                    else entry
+                )
                 for entry in preprocessed
             ]
         else:
-            pass
-    return JsonObject(**obj)
+            reprocessed = preprocessed
+        dst_obj[new_key] = reprocessed
+    return JsonObject(dst_obj)
